34d1c5c6220802b48540922f61cd0068665a8ac1
Ugochi Ihen <ugihen.k@gmail.com>
2023-03-31 00:40:01 +0100

    Madlib!

---
+++ madlib.py
@@ -0,0 +1,13 @@
+name = input("Enter a name: \n")
+food = input("Enter any eadibles: \n")
+verb = input("Enter any verb:(i.e. has to be a doing word) \n")
+item = input("Enter an item: \n")
+cartoon_ch = input("Enter a cartoon character: \n")
+place = input ("Enter any place: \n")
+liquid = input("Enter the name of any liquid: \n") 
+famous_person = input("Enter the name of a famous person: \n")
+
+print("\n")
+print("It is okay", name.title(), "to have", food, "once every 6 hours before", verb, "on your", item, 
+"irrespective having the beauty and build of", cartoon_ch.title(), ". Get back to your", place.capitalize(), "have some", 
+liquid, "and live free as much as", len(famous_person), "times like", famous_person, "!")
